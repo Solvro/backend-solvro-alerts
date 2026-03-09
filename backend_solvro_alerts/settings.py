08cb2a0873d610d57709d1f2b0a079b6b7e04a65
WiktorGruszczynski <wgwiktorwg@gmail.com>
2026-03-09 12:47:25 +0100

    feat: init commit alert model

--- backend_solvro_alerts/settings.py
+++ backend_solvro_alerts/settings.py
@@ -55,6 +55,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "drf_spectacular",
+    "applications",
+    "alerts",
 ]
 
 MIDDLEWARE = [
